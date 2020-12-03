78cc3cc9b817297defd7c64edcb7cda0919136da
jesuscovam <jesuscovam@icloud.com>
2020-12-02 20:33:59 -0500

    primer commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Nav from "../components/nav";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className="text-white p-5 text-xl">
+        Hola aqui vamos a poner un link
+      </h1>
+    </div>
+  );
+}
